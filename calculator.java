23088a01947ee680af7967c7a4ea0bb47a07007e
Ahad Khattak <61601300+AhadKhattak122344@users.noreply.github.com>
2024-07-21 10:35:49 -0400

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,16 @@
+
+package calculator;
+
+/**
+ *
+ * @author JJ Posti - techtimejourney.net.
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Gui gui = new Gui();
+    gui.setVisible(true);}
+}
